cbb3def65707dabc837c024df6e4d3e27b7f81c5
smrrazavian <smrrazavian@outlook.com>
2022-05-17 20:52:27 +0430

    :tada: Initial commit

---
+++ child_abuse_detection/__init__.py
@@ -0,0 +1,15 @@
+# type: ignore[attr-defined]
+"""child abuse detection based on python and bag of words."""
+
+import sys
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
