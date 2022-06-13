c2e8b7511357eb0f61d40ab252c3105d1c485f1d
smrrazavian <smrrazavian@outlook.com>
2022-06-13 13:48:17 +0430

    :sparkles: Add vectorizer function with scikit learn.

--- child_abuse_detection/Vectorization.py
+++ child_abuse_detection/Vectorization.py
@@ -1,4 +1,24 @@
 """Vectorization module."""
+import numpy as np
 from gensim import models
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 from child_abuse_detection import stopword
+
+
+def countVectorizer(text) -> np.array:
+    """
+    Args:
+
+    Returns:
+
+    """
+    vectorizer = CountVectorizer()
+    textVector = vectorizer.fit_transform(text).toarray()
+    return textVector
+
+
+def tfidfVectorizer(text):
+    vectorizer = TfidfVectorizer()
+    textVector = vectorizer.fit_transform(text).toarray()
+    return textVector
