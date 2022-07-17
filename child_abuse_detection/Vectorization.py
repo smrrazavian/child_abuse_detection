6f641e078a44213165cbb0c553fd4ebd48b09c37
smrrazavian <smrrazavian@outlook.com>
2022-07-17 14:13:35 +0430

    :hankey: Writing  Vectorizer modules but still need to refactor

--- child_abuse_detection/Vectorization.py
+++ child_abuse_detection/Vectorization.py
@@ -1,15 +1,35 @@
 """Vectorization module."""
+from typing import List
+
 import numpy as np
-from gensim import models
+from gensim.models import Word2Vec
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 from child_abuse_detection import stopword
 
+# TODO:
+#  Config countVectorizer & tfidfVectorizer. Complete word2vec.
 
-def countVectorizer(text) -> np.array:
+
+def word2vec(text: List[str]) -> np.array:
     """
     Args:
+        text (List[str]):
+    Returns:
 
+    """
+    w2vModel = Word2Vec(sentences=text, alpha=0.05, min_alpha=0.0007)
+    w2vModel.save("word2vec.model")
+    w2vModel.train(
+        text, total_examples=w2vModel.corpus_count, epochs=30, report_delay=1
+    )
+    return w2vModel.load
+
+
+def countVectorizer(text: List[str]) -> np.array:
+    """
+    Args:
+        text (List[str]):
     Returns:
 
     """
@@ -18,7 +38,13 @@ def countVectorizer(text) -> np.array:
     return textVector
 
 
-def tfidfVectorizer(text):
+def tfidfVectorizer(text: List[str]) -> np.array:
+    """
+    Args:
+
+    Returns:
+
+    """
     vectorizer = TfidfVectorizer()
     textVector = vectorizer.fit_transform(text).toarray()
     return textVector
