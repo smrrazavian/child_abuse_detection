4cc9b591abf3fa366c803fac49dbe6defad376da
smrrazavian <smrrazavian@outlook.com>
2022-07-20 15:04:34 +0430

    :hammer: Eddit pars_csv and clean_text modules and replace dataframe instead of list

--- child_abuse_detection/text_utils.py
+++ child_abuse_detection/text_utils.py
@@ -17,10 +17,14 @@ def pars_csv(filename: str) -> pd.DataFrame:
             gets a csv filename in assets directory
     Returns:
         df_data (pd.DataFrame):
-            Pandas data-frame of given csv file
+            Pandas data-frame of the given csv file
     """
     cwd = Path.cwd()
     file_path = os.path.join((cwd / "./assets/").resolve(), filename + ".csv")
+    is_abuse_dict = {
+        "ChildAbuse": 1,
+        "SampleNews": 0,
+    }
     # --------Making data-frame from csv file--------
     df_data = pd.read_csv(file_path)
     df_data = df_data.dropna(how="any", axis=0)
@@ -34,28 +38,34 @@ def pars_csv(filename: str) -> pd.DataFrame:
         non_unicode_text = non_unicode_text.replace("\xa0", " ")
         non_unicode_text = non_unicode_text.replace("\u200e", "")
         df_data["content"][i] = non_unicode_text
+    df_data.drop_duplicates(inplace=True)
+    df_data = df_data[(df_data.content != "\n")]
+    df_data["is_childAbuse"] = [is_abuse_dict[filename]] * len(df_data)
+    df_data.drop(columns=["title"], inplace=True)
+    df_data.reset_index(inplace=True, drop=True)
     return df_data
 
 
-def clean_text(texts: List[str]) -> List[str]:
-    # TODO Union type checking
+def clean_text(df_data: pd.DataFrame) -> pd.DataFrame:
     """Deletes stopwords, punctuations and digits of text.
     Args:
-        texts (List[str]):
-            a list of newses.
+        df_data (pd.DataFrame):
+            a data-frame of newses.
     Returns:
-        withoutStopWords (List[str]):
-            a list of newses without StopWords.
+        df_data (pd.DataFrame):
+            cleaned data-frame of df-data.
     """
-    without_stop_words = []
-    for text in texts:
+    without_stop_words = []  # TODO Ask if is it efficient to create new list?
+    contents = df_data["content"]
+    for text in contents:
         text = "".join(filter(lambda x: not x.isdigit(), text))
         puncts = "!\"#%'()*+,-./:;<=>?@\\[\\]^_`{|}~’”“′‘\\\\]؟؛«»،٪"
         text = text.translate(str.maketrans("", "", puncts))
         stop_words = set(stopwords_list())
         text = " ".join([word for word in text.split() if word not in stop_words])
         without_stop_words.append(text)
-    return without_stop_words
+    df_data["content"] = without_stop_words
+    return df_data
 
 
 def tokenize_text(texts: List[str], tokenize_type: str = "word") -> Any:
