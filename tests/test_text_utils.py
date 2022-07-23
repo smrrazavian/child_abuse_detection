e1f12e64f46bd468419d9ad60905e3ca3eced791
smrrazavian <smrrazavian@outlook.com>
2022-07-23 16:01:28 +0430

    :heavy_check_mark: Pass path_finder test but needs to fix the mypy

--- tests/test_text_utils.py
+++ tests/test_text_utils.py
@@ -1,1 +1,20 @@
 """Unit tests for stopword module."""
+import pytest
+
+from child_abuse_detection.text_utils import path_finder
+
+
+def test_path_finder():
+    """Test path_finder"""
+    path = path_finder("ChildAbuse")
+    assert (
+        path
+        == "/home/smrrazavian/Documents/Personal-Projects/child_abuse/child_abuse_detection/assets/ChildAbuse.csv"
+    )
+
+
+def test_path_finder_fail():
+    """Test if path_finder is failing"""
+    with pytest.raises(Exception) as error:
+        path_finder("childabuse")
+        assert error.value.args[0] == "Incorrect file name."
